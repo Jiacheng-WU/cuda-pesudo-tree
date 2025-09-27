127a759d458d5cd6aa4bdd23406c1ca1bc502d9a
Jiacheng Wu <jcwu22@cs.washington.edu>
2025-09-27 14:15:40 -0700

    Add More Dependencies
    Update Rust Configuration
    
    Add Triton
    
    Update PyTest
    
    Update gitignore

---
+++ python/test/test.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_example(self):
+        self.assertEqual(1 + 1, 2)
+
+
+# This is an example test file.
+# You can run this test using a test framework like unittest or pytest.
+# Make sure to have the necessary dependencies installed.
+# For example, you can run this test with the command:
+# python -m unittest test.py
+# or
+# pytest test.py
